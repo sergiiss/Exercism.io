c1e13dc58518180739be8d07743e5b10fd425176
sergiiss <dracula23@mail.ru>
2017-02-04 16:19:59 +0300

    Small code improvements

--- lib/perfect_number.rb
+++ lib/perfect_number.rb
@@ -1,23 +1,23 @@
 class PerfectNumber
   attr_reader :number
-    
+
   def initialize(number)
     @number           = number
-    
+
     @list_of_numbers  = []
     @divisors_sum     = 0
   end
 
   def determine_the_number_of_perfect
     calculate_sum_of_divisors
-    
+
     print_result
   end
 
   private
-  
+
   attr_reader :divisors_sum, :list_of_numbers
-  
+
   def calculate_sum_of_divisors
     create_a_list_of_numbers
     list_of_numbers.each do |num|
@@ -26,26 +26,26 @@ class PerfectNumber
   end
 
   def create_a_list_of_numbers
-    index = 1    
-    
+    index = 1
+
     while index < number.to_i
       @list_of_numbers << index
-      
+
       index += 1
     end
   end
-  
+
   def conditional_test(num)
     @divisors_sum += num if number % num == 0
   end
-  
+
   def print_result
     if divisors_sum == number
-      puts "This is perfect number"
-      "This is perfect number"
+      puts 'This is perfect number'
+      'This is perfect number'
     else
-      puts "This is not a perfectum number"
-      "This is not a perfectum number"
+      puts 'This is not a perfectum number'
+      'This is not a perfectum number'
     end
   end
 end
--- lib/sieve.rb
+++ lib/sieve.rb
@@ -3,36 +3,36 @@ class Sieve
 
   def initialize(limit_the_number)
     @limit_the_number     = limit_the_number
-    
+
     @quantity_of_dividers = 0
     @dividers             = []
     @primes               = []
     @numbers              = []
   end
-  
+
   def sift_numbers
     receive_a_sequence_of_numbers
     receive_a_prime_numbers
     create_prime_numbers
     printer
-    
+
     primes
   end
-  
+
   private
-  
+
   attr_reader :dividers, :primes, :numbers, :quantity_of_dividers
 
   def receive_a_sequence_of_numbers
     index = 2
-    
+
     while index <= limit_the_number
       @numbers << index
-      
+
       index += 1
     end
   end
-  
+
   def receive_a_prime_numbers
     numbers.each do |number|
       @dividers << number
@@ -40,20 +40,20 @@ class Sieve
       @quantity_of_dividers = 0
     end
   end
-  
+
   def find_prime_numbers(number)
     dividers.each do |divider|
       break if divider == number
       @quantity_of_dividers += 1 if number % divider == 0
     end
-    
+
     @primes << number if quantity_of_dividers == 0
   end
-  
+
   def create_prime_numbers
-    @primes = primes.join(" ")
+    @primes = primes.join(' ')
   end
-  
+
   def printer
     puts primes
   end
